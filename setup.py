54f6262a93d81742438647a0e7b000ed09cb862d
phwhitin <phwhitin@cisco.com>
2017-04-06 09:42:37 -0400

    changed version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='app-utilities',
-      version='0.0.1',
+      version='0.0.2',
       author='Philip Whiting',
       author_email='phwhitin@cisco.com',
       packages=['app_util'],
